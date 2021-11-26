d73ad28f01604eaad14273019eee789460dcf717
NicholasGhartey <nrghartey@gmail.com>
2021-11-26 14:41:06 +0000

    Test.sh modifications

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "Test stage"
-# sudo apt install python3.8-venv
-# python3 -m venv venv
-pip install pytest
-# source venv/bin/activate
+sudo apt install python3.8-venv
+python3 -m venv venv
+source venv/bin/activate
+pip3 install pytest
+pip3 -r requirements.txt
 python3 -m pytest \
    --cov=application \
    --cov-report term-missing \
