77052835b6240fce192f4812f5a101b842eb9c97
NicholasGhartey <nrghartey@gmail.com>
2021-11-26 16:28:11 +0000

    routes.py  modifications

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -35,12 +35,11 @@ def update_task(id):
 
     return render_template('update_task.html', task=task, form=form)
 
-# @app.route('/delete/task/<int:id>')
-# def delete_task(id):
-#     task = Tasks.query.get(id)
-#     db.session.delete(task)
-#     db.session.commit()
-#     return redirect(url_for('home'))
+@app.route('/delete/task/<int:id>')
+def delete_task(id):
+    response = requests.delete(f"http://{backend_host}/delete/task/{id}").json()
+
+    return redirect(url_for('home'))
 
 # @app.route('/complete/task/<int:id>')
 # def complete_task(id):
