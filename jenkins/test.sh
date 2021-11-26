ce26be57e3d9249d70ebe0768c7ac8c8dd7f4f3f
NicholasGhartey <nrghartey@gmail.com>
2021-11-26 17:19:06 +0000

    routes.py  modifications

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Test stage"
-# sudo apt install -y python3.8-venv
-# python3 -m venv venv
-# source venv/bin/activate
-# pip3 install pytest
-# pip3 install -r requirements.txt
-# python3 -m pytest \
+sudo apt install -y python3.8-venv
+python3 -m venv venv
+source venv/bin/activate
+pip3 install pytest
+pip3 install -r requirements.txt
+python3 -m pytest \
    # --cov=application \
    # --cov-report term-missing \
    # --cov-report xml:coverage.xml \
